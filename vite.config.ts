d441ba375514b5b8e634ca5a916613d460583d74
leilei405 <1601178425@qq.com>
2024-11-20 00:30:01 +0800

    feat: ✨vite.config 文件配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': 'http://api-driver.marsview.cc'
+    }
+  },
+
+  // 配置路径别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
