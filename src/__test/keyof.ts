b1410e2106664aafe55e4256c2ca6ffc1fe907a7
leilei405 <1601178425@qq.com>
2024-11-20 21:49:14 +0800

    feat: ✨解读tsconfig配置文件  安装 路由包

---
+++ src/__test/keyof.ts
@@ -0,0 +1,17 @@
+interface Person {
+  name: string
+  age: number
+  location: string
+}
+type K1 = keyof Person // 'name' | 'age' | 'location'
+const k: K1 = 'name'
+
+console.log(k)
+
+const info = {
+  name: '张三',
+  age: 18,
+  location: '北京'
+}
+
+type User = typeof info
