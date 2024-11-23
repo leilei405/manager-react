049d2eabdbe6185b2bb3d552911a1d437a33ca6b
leilei405 <1601178425@qq.com>
2024-11-23 12:53:54 +0800

    style: 💄自定义主题色

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import router from './router'
 // 2. 导入路由组件  <BrowserRouter> <Router /> </BrowserRouter>
 // import Router from './router'
 
+import { ConfigProvider } from 'antd'
+
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#a170f2'
+        }
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  )
 }
 
 export default App
