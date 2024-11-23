d5c171369e3efa0738267ea9653bccff2b5167aa
leilei405 <1601178425@qq.com>
2024-11-23 12:34:50 +0800

    fix: 🐛删除废弃代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-// @ts-ignore
-import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 // 1. 导入路由对象 <RouterProvider router={router} />
 import router from './router'
-
 // 2. 导入路由组件  <BrowserRouter> <Router /> </BrowserRouter>
 // import Router from './router'
 
